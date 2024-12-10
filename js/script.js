e13afd98f50932b3f809c692d3b7653f6113630f
Luis Enrique Damian Palacios <120479327+LuisDamianP@users.noreply.github.com>
2024-12-10 01:42:45 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const field = document.getElementById('tulipField');
+
+function createTulip(x, y) {
+    const tulip = document.createElement('div');
+    tulip.classList.add('tulip');
+    tulip.style.left = `${x}px`;
+    tulip.style.bottom = `${y}px`;
+    return tulip;
+}
+
+function generateTulips() {
+    for (let i = 0; i < 100; i++) {
+        const x = Math.random() * window.innerWidth;
+        const y = Math.random() * (window.innerHeight / 2);
+        const tulip = createTulip(x, y);
+        field.appendChild(tulip);
+    }
+}
+
+generateTulips();
+
+window.addEventListener('resize', () => {
+    field.innerHTML = '';
+    generateTulips();
+});
